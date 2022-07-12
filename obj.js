59095635d81f7f760865fdc649d881058feb765c
FILIPE SOUSA DA SILVA <79103757+euFilpeSilva@users.noreply.github.com>
2022-07-12 16:52:33 -0300

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,13 +1,13 @@
 let amigo = {
    nome: 'Filipe',
-   sexo: 'F',
+   sexo: 'M',
    peso: 55.6,
-// quando definimos um valor padrão numa variavel sendo passada num parametro
+// quando definimos um valor padrão numa variavel passada como parametro
 // é uma maneira de evitar problemas, ou seja, se não passarmos valor nenhum nos
 // parametros, o valor sera aquele que esta pré definido na variavel que se encontra
 // na area de parametros da função
 
-// função que pega o peso que eu vou passar la em baixo e passa como argumento atraves do this
+// função que pega o peso e passa como argumento atraves do this
 // somando ao peso atual
 engordar(p = 0) {
     console.log('Engordou')
